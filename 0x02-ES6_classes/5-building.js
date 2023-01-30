143fbb71c7ac1f4375bb4a15714f9cb932c4d1d2
Mulugeta Achiso <anem1351@gmail.com>
2023-01-30 20:17:45 +0000

    cnfg file

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,17 +1,23 @@
+/* eslint-disable */
+
 export default class Building {
   constructor(sqft) {
-    // Any class extending Building must implement below method
-    if (this.constructor !== Building && !this.evacuationWarningMessage) throw Error('Class extending Building must override evacuationWarningMessage');
-
-    // Create objs
-    this._sqft = sqft;
+    this.sqft = sqft;
+    if (
+      this.constructor !== Building &&
+      this.evacuationWarningMessage === undefined
+    ) {
+      throw Error(
+        "Class extending Building must override evacuationWarningMessage"
+      );
+    }
   }
 
-  // Methods
-
-  // Setters
-
-  // Getters
   get sqft() {
     return this._sqft;
   }
+
+  set sqft(value) {
+    this._sqft = value;
+  }
+}
