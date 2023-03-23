349703fcebc33f4907bf4008d301e16646b7a1be
Mulugeta Achiso <anem1351@gmail.com>
2023-03-23 10:17:21 +0000

    api.js

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -3,5 +3,5 @@ const express = require('express');
 const app = express();
 const port = 7865;
 
-app.get('/', (req, res) => res.end('Welcome to the payment system'))
+app.get('/', (req, res) => res.send('Welcome to the payment system'))
 app.listen(port, () => console.log(`API available on localhost port ${port}`));
